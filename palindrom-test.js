3bf6a3d012962c3d8feb6b10eb53acb8b0391660
Wahyu Ramadan <wramadan1203@gmail.com>
2023-06-27 23:32:01 +0700

    comment single palindrom

--- palindrom-test.js
+++ palindrom-test.js
@@ -1,13 +1,16 @@
 const singlePalindrome = (palindromParam) => {
+  // Set string to lowercase
   const setToLowerCaseWord = palindromParam.toLowerCase();
-  const reversWord = palindromParam.toLowerCase().split("").reverse().join("");
+  // Split the strings by word, reverse them, and concatenate them
+  const reversWord = setToLowerCaseWord.split("").reverse().join("");
+  // Set default result to false
   let result = false;
-
+  // Check if reverse word is same with original string when already set to lowercase and set to true
   if (reversWord === setToLowerCaseWord) {
     result = true;
   }
 
-  console.log(`string: ${palindromParam}`);
+  console.log(`string: ${setToLowerCaseWord}`);
   console.log(`single palindrom: ${result}`);
   console.log("=========================");
 };

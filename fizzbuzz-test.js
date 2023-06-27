8d23d1a3c3e429a40cff83127c9d68ae4340fccb
Wahyu Ramadan <wramadan1203@gmail.com>
2023-06-27 22:59:20 +0700

    add comment

--- fizzbuzz-test.js
+++ fizzbuzz-test.js
@@ -2,12 +2,16 @@ const FizzBuzz = () => {
   const range = 100;
 
   for (let i = 1; i <= range; i++) {
+    // If the number series is a multiple of 3 and 5 then display FizzBuzz
     if (i % 3 === 0 && i % 5 === 0) {
       console.log("FizzBuzz");
+      // If the number series is a multiple of 5 then display Buzz
     } else if (i % 5 === 0) {
       console.log("Buzz");
+      // If the number series is a multiple of 3 then display Fizz
     } else if (i % 3 === 0) {
       console.log("Fizz");
+      // If not contain multiple 3 or 5 or 3 and 5 then display number
     } else {
       console.log(i);
     }

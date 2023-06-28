b3779ef275a463c256f1636887fecdf381cdbd59
Wahyu Ramadan <wramadan1203@gmail.com>
2023-06-28 12:52:36 +0700

    multiple word palindrom

--- palindrom-test.js
+++ palindrom-test.js
@@ -1,4 +1,4 @@
-const singlePalindrome = (palindromParam) => {
+const singleWord = (palindromParam) => {
   // Set string to lowercase
   const setToLowerCaseWord = palindromParam.toLowerCase();
   // Split the strings by word, reverse them, and concatenate them
@@ -15,7 +15,79 @@ const singlePalindrome = (palindromParam) => {
   console.log("=========================");
 };
 
-singlePalindrome("SAIPPUAKIVIKAUPPIAS");
-singlePalindrome("Aibohphobia");
-singlePalindrome("Anna");
-singlePalindrome("Civic");
+const multipleWord = (palindromParam) => {
+  // Set string to lowercase
+  const setToLowerCaseWord = palindromParam.toLowerCase();
+  // Set result
+  let result = false;
+
+  // If word contain comma
+  if (setToLowerCaseWord.indexOf(",") > -1) {
+    // Set default array to word contain comma
+    let wordComma = [];
+
+    setToLowerCaseWord.split(",").map((value) => {
+      // Remove first space
+      const removeFirstSpace = value.trim();
+      // Find index of space
+      const spaceIndex = removeFirstSpace.indexOf(" ");
+      // Reverse word
+      const reverse = removeFirstSpace
+        .replace(/\s/g, "")
+        .split("")
+        .reverse()
+        .join("");
+      // Add space
+      const addSpace = `${reverse.substring(0, spaceIndex)} ${reverse.substring(
+        spaceIndex,
+        reverse.length
+      )}`;
+      // Insert in array
+      wordComma.push(addSpace);
+    });
+
+    // Join the word with separate ,
+    const resultWord = wordComma.join(", ");
+
+    // Check if the word is palindrom or not
+    if (resultWord === setToLowerCaseWord) {
+      result = true;
+    } else {
+      result = false;
+    }
+
+    console.log(`string: ${setToLowerCaseWord}`);
+    console.log(`multiple palindrom: ${result}`);
+    console.log("=========================");
+    return;
+  }
+
+  // Find space index
+  const spaceIndex = setToLowerCaseWord.indexOf(" ");
+  // Remove all space
+  const removaAllSpace = setToLowerCaseWord.replace(/\s/g, "");
+  // Reverse word
+  const reverse = removaAllSpace.split("").reverse().join("");
+
+  // Fill space by index
+  const resultWord = `${reverse.substring(0, spaceIndex)} ${reverse.substring(
+    spaceIndex,
+    reverse.length
+  )}`;
+
+  if (resultWord === setToLowerCaseWord) {
+    result = true;
+  }
+
+  console.log(`string: ${setToLowerCaseWord}`);
+  console.log(`multiple palindrom: ${result}`);
+  console.log("=========================");
+};
+
+singleWord("SAIPPUAKIVIKAUPPIAS");
+singleWord("Aibohphobia");
+singleWord("Anna");
+singleWord("Civic");
+
+multipleWord("No lemon, no melon");
+multipleWord("My gym");
